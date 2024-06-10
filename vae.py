445a53b442f5396ba8f1f0e43ccb949a0b90e2be
Hanksome1 <wu20040814@gmail.com>
2024-06-10 18:26:04 +0800

    VAE python model commit

---
+++ vae.py
@@ -0,0 +1,149 @@
+# include necessary libraries
+import torch
+import numpy as np
+import torch.nn as nn
+from torch.optim import Adam
+import matplotlib.pyplot as plt
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from mpl_toolkits.axes_grid1 import ImageGrid
+from torchvision.utils import save_image, make_grid
+
+# create a transofrm to apply to each datapoint
+transform = transforms.Compose([transforms.ToTensor()])
+
+# download the MNIST datasets
+path = '~/datasets'
+train_dataset = MNIST(path, transform=transform, download=True)
+test_dataset  = MNIST(path, transform=transform, download=True)
+
+# create train and test dataloaders
+batch_size = 100
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class VAE(nn.Module):
+
+    def __init__(self, input_dim=784, hidden_dim=400, latent_dim=200, device=device, output_dim=196):
+        super(VAE, self).__init__()
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(output_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, latent_dim),
+            nn.LeakyReLU(0.2)
+            )
+        
+        # latent mean and variance 
+        self.mean_layer = nn.Linear(latent_dim, 2)
+        self.logvar_layer = nn.Linear(latent_dim, 2)
+        self.pool = nn.MaxPool2d(2)        
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(2, latent_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(latent_dim, hidden_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(hidden_dim, output_dim),
+            nn.Sigmoid()
+            )
+     
+    def encode(self, x):
+        x = self.encoder(x)
+        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
+        return mean, logvar
+
+    def reparameterization(self, mean, var):
+        epsilon = torch.randn_like(var).to(device)      
+        z = mean + var*epsilon
+        return z
+
+    def decode(self, x):
+        return self.decoder(x)
+
+    def forward(self, x):
+        x1 = self.pool(x)
+        x2 = x1.view(batch_size, 196)  
+        mean, logvar = self.encode(x2)
+        z = self.reparameterization(mean, logvar)
+        x_hat = self.decode(z)
+        return x2, x_hat, mean, logvar
+
+model = VAE().to(device)
+optimizer = Adam(model.parameters(), lr=1e-3)
+
+def loss_function(x, x_hat, mean, log_var):
+    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
+    KLD = - 0.5 * torch.sum(1+ log_var - mean.pow(2) - log_var.exp())
+
+    return reproduction_loss + KLD
+
+def train(model, optimizer, epochs, device, x_dim=784):
+    model.train()
+    for epoch in range(epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            x = x.to(device)
+
+            optimizer.zero_grad()
+
+            x2, x_hat, mean, log_var = model(x)
+            loss = loss_function(x2, x_hat, mean, log_var)
+            
+            overall_loss += loss.item()
+            
+            loss.backward()
+            optimizer.step()
+
+        print("\tEpoch", epoch + 1, "\tAverage Loss: ", overall_loss/(batch_idx*batch_size))
+    return overall_loss
+train(model, optimizer, epochs=50, device=device)
+
+def plot_latent_space(model, scale=5.0, n=25, digit_size=14, figsize=15):
+    # display a n*n 2D manifold of digits
+    figure = np.zeros((digit_size * n, digit_size * n))
+
+    # construct a grid 
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = torch.tensor([[xi, yi]], dtype=torch.float).to(device)
+            x_decoded = model.decode(z_sample)
+            digit = x_decoded[0].detach().cpu().reshape(digit_size, digit_size)
+            figure[i * digit_size : (i + 1) * digit_size, j * digit_size : (j + 1) * digit_size,] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    plt.title('VAE Latent Space Visualization')
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("mean, z [0]")
+    plt.ylabel("var, z [1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
+    plt.savefig("vae.png")
+
+plot_latent_space(model, scale=1.0)
+torch.save(model.state_dict(), 'model.pth')
+
+import numpy as np
+for param_tensor in model.state_dict():
+  print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+  model_cpu = model.state_dict()[param_tensor].cpu()
+  values = list(model_cpu.numpy().flatten())
+  filename = param_tensor + '.txt'
+  txt_file = open(filename, 'w')
+  for v in values:
+    txt_file.write(str(v)+'\n')
+txt_file.close()
+
